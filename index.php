667ddfcd1b8e59a0e2dac03dddc43f93d8b0b038
yabe <abe.yuki@zynas.co.jp>
2017-03-08 18:36:33 +0900

    fix!!!

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header("Content-type: text/html; charset=utf-8");
 _無限にもつを食べるあべおくん();
 
 function _無限にもつを食べるあべおくん() {
-    echo str_repeat('(´⊙ω⊙`)', 10000);
+    echo str_repeat('(´⊙ω⊙`)', 1000);
     exit;
 }
 ?>
